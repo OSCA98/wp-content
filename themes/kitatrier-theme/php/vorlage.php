81db011bca186f7ba0e388ac57f3abbb3546103e
Jeremy Koch <jeremy.koch5@gmail.com>
2023-07-12 20:25:43 +0200

    Templates Init

--- themes/kitatrier-theme/php/vorlage.php
+++ themes/kitatrier-theme/php/vorlage.php
@@ -1,46 +1,13 @@
 <?php
 /*
-Template Name: Vorlage
+Template Name: Default
 */
 get_header();
 ?>
 
-<div class="contentmain">
-    <div class="content-wrap">
+<div class="content">
 
-        <?php the_content(); ?>
-        
-        <div id="col-right">
-
-            <div>
-                <div id="cr-1" class="frame-beigebox">
-                    <?php
-                    global $schneidershof;
-                    erzeuge_Kitainfo($schneidershof);
-                    ?>
-                </div>
-            </div>
-
-            <div>
-                <div id="cr-2" class="frame-beigebox">
-                    <?php
-                    global $imTreff;
-                    erzeuge_Kitainfo($imTreff);
-                    ?>
-                </div>
-            </div>
-
-            <div>
-                <div id="download"><!--Download-->
-                    <?php
-                    erzeuge_html_pdf_inhalt();
-                    ?>
-                </div>
-            </div>
-
-        </div>
-        <div style="clear:both;"></div>
-    </div>
+    <?php the_content(); ?>
 
 </div>
 
