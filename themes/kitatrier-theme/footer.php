f0c089b09d1be9d5e266821bb923841991204c55
yayliu <uyayli@msn.com>
2023-07-18 01:47:43 +0200

    js: Ersetze Obermenü <a> href ,mit dem ersten Untermenü <a> href

--- themes/kitatrier-theme/footer.php
+++ themes/kitatrier-theme/footer.php
@@ -11,16 +11,36 @@
 
 ?>
 
-	<footer>
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'footer-menu',
-			)
-		);
-		?>
-	</footer><!-- #colophon -->
+<footer>
+	<?php
+	wp_nav_menu(
+		array(
+			'theme_location' => 'footer-menu',
+		)
+	);
+	?>
+</footer><!-- #colophon -->
 </div><!-- #page -->
+<script>
+	document.addEventListener("DOMContentLoaded", function () {
+
+		// Selektiere die ersten li von den jeweiligen untermenüs
+		var listItems = document.querySelectorAll('footer div.menu ul > li > ul >li:first-child');
+
+		listItems.forEach(function (item) {
+			// Überprüfe, ob ein über-übergeordnetes Element existiert
+			if (item.parentNode.parentNode) {
+				var grandparentElement = item.parentNode.parentNode;
+				//console.log('ELTERN: ',grandparentElement);
+				//console.log(grandparentElement.firstChild,'-------------',item.firstChild);
+
+				// Ersetze Obermenü <a> href, mit dem ersten Untermenü <a> href
+				grandparentElement.firstChild.setAttribute('href', item.firstChild.getAttribute('href'));
+			}
+		});
+	});
+</script>
 
 </body>
+
 </html>
